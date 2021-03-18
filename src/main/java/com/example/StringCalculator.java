200d38ae82b77c1f7b81b7d3216b0faa7b8726a8
Ola Lindgård <ola.lindgard@iths.se>
2021-03-18 08:41:23 +0100

    Se om det funkar nu...

---
+++ src/main/java/com/example/StringCalculator.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class StringCalculator
+{
+
+    public int add(String numbers)
+    {
+        return 1;
+    }
+
+    public static void main(String[] args)
+    {
+        StringCalculator sc = new StringCalculator();
+        int result = sc.add("1,4");
+    }
+}
